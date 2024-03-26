306eeebb399334ab39c9998ed5446427cf4832db
NyamburaMichelle <162292326+NyamburaMichelle@users.noreply.github.com>
2024-03-26 14:07:04 +0300

    Update detector.js

--- detector.js
+++ detector.js
@@ -1,15 +1,18 @@
 // Checks on car speed
 
 
-//const speed =1000
+const speed =1000
 
 function speedDetector(speed) {
+    let difference=(speed-7)/5
     if (speed<= 70) {
         console.log("OK")
     }else if (speed>70 && speed<130) {
         console.log(`Points: ${(speed-70)/5}`)
         //12 points are exceeded at a speed above 130
-    }else if (speed>130) {
+    }else if (difference>12) {
         console.log("License suspended")
     }
 }
+
+speedDetector(speed)
